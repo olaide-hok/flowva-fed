b51a738a223a03c4f51babd650bc28e27cf19fcf
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-21 14:16:40 +0100

    forgot password page

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,30 @@
+import {Metadata} from 'next';
+import {APP_NAME} from '@/lib/constants';
+import {auth} from '@/auth';
+import {redirect} from 'next/navigation';
+import CustomSvgs from '@/components/shared/custom-svgs';
+import ForgotPasswordForm from './forgot-password-form';
+
+export const metadata: Metadata = {
+    title: 'Forgot Password',
+};
+
+const ForgotPassword = async () => {
+    const session = await auth();
+
+    if (session) {
+        return redirect('/');
+    }
+
+    return (
+        <div className="container">
+            <div className="flex flex-center mb-[30px] font-bold text-2xl text-(--primary-fb)">
+                <CustomSvgs type="logo" className="mr-2.5 w-7 h-7" />
+                {APP_NAME}
+            </div>
+            <ForgotPasswordForm />
+        </div>
+    );
+};
+
+export default ForgotPassword;
