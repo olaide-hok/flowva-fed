2432e5c3daa390d30d87d681c2e8e982005eff29
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-21 20:16:39 +0100

    update sign-form to display errors and success messages

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 import {Button} from '@/components/ui/button';
@@ -6,14 +5,15 @@ import {Input} from '@/components/ui/input';
 import {Label} from '@/components/ui/label';
 import {signInDefaultValues} from '@/lib/constants';
 import Link from 'next/link';
-import {useActionState, useState} from 'react';
+import {useActionState, useEffect, useState} from 'react';
 import {useFormStatus} from 'react-dom';
 import {signInWithCredentials} from '@/lib/actions/user.actions';
 import CustomSvgs from '@/components/shared/custom-svgs';
-import GoogleSignInButton from './googleSignIn';
+// import GoogleSignInButton from './googleSignIn';
 import {usePasswordToggle} from '@/hooks/usePwdToggle';
+import {redirect} from 'next/navigation';
 
-const CredentialsSignInForm = ({session}: any) => {
+const CredentialsSignInForm = () => {
     const [data, action] = useActionState(signInWithCredentials, {
         success: false,
         message: '',
@@ -21,12 +21,22 @@ const CredentialsSignInForm = ({session}: any) => {
 
     const [, setPassword] = useState('');
     const {inputType, toggleLabel, toggle} = usePasswordToggle();
+    useEffect(() => {
+        if (data?.success && data?.redirectTo) {
+            setTimeout(() => {
+                redirect(data.redirectTo);
+            }, 5000);
+        }
+    }, [data]);
 
     const SignInButton = () => {
         const {pending} = useFormStatus();
-
         return (
-            <Button disabled={pending} className="btn" variant="default">
+            <Button
+                type="submit"
+                disabled={pending}
+                className="btn"
+                variant="default">
                 <CustomSvgs type="sign-in" />
                 {pending ? 'Signing In...' : 'Sign In'}
             </Button>
@@ -39,9 +49,17 @@ const CredentialsSignInForm = ({session}: any) => {
                 Welcome back
             </div>
 
-            {data && !data.success && (
-                <div className="form-message">{data.message}</div>
+            {data && !data.success && data.message !== '' ? (
+                <div className="form-message error-message">{data.message}</div>
+            ) : (
+                ''
             )}
+            {data && data.success && (
+                <div className="form-message success-message">
+                    {data.message}
+                </div>
+            )}
+
             <div className="form-group">
                 <Label htmlFor="email">Email</Label>
                 <Input
@@ -85,7 +103,7 @@ const CredentialsSignInForm = ({session}: any) => {
 
             <div className="divider">or continue with</div>
 
-            <GoogleSignInButton session={session} />
+            {/* <GoogleSignInButton session={session} /> */}
 
             <div className="form-footer">
                 Don&apos;t have an account?{' '}
