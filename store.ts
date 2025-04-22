e09792fd613edf69140390d8c8f534a87977d7bf
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-22 17:55:04 +0100

    create useOnboardingAppState store using zustand

---
+++ store.ts
@@ -0,0 +1,69 @@
+import {create} from 'zustand';
+import {
+    AboutYou,
+    Goals,
+    Location,
+    OnboardingFormData,
+    ToolStack,
+} from './lib/validators';
+
+type FlowvaOnboardingAppState = OnboardingFormData & {
+    step: number;
+    nextStep: () => void;
+    getTotalSteps: () => number;
+    setAboutYou: (aboutYou: AboutYou) => void;
+    setLocation: (location: Location) => void;
+    setToolStack: (tool: ToolStack) => void;
+    setGoals: (goals: [Goals]) => void;
+    submitForm: () => void;
+};
+
+// --- Zustand Store ---
+export const useOnboardingAppState = create<FlowvaOnboardingAppState>(
+    (set, get) => ({
+        // Initial form state
+        step: 0,
+        aboutYou: {
+            role: undefined,
+            work: [],
+            work_other: '',
+        },
+        location: {
+            country: undefined,
+        },
+        toolStack: {
+            tools: [],
+        },
+        goals: {
+            goals: [],
+        },
+
+        // Step logic
+        nextStep: () => {
+            const {step} = get();
+            const total = get().getTotalSteps();
+            if (step < total) {
+                set({step: step + 1});
+            }
+        },
+
+        getTotalSteps: () => 5, // step 5 is the welcome screen
+
+        // Setters
+        setAboutYou: (aboutYou) => set({aboutYou}),
+        setLocation: (location) => set({location}),
+        setToolStack: (toolStack) => set({toolStack}),
+        setGoals: (goals) => set({goals}),
+
+        // Final Submission
+        submitForm: () => {
+            const data: OnboardingFormData = {
+                aboutYou: get().aboutYou,
+                location: get().location,
+                toolStack: get().toolStack,
+                goals: get().goals,
+            };
+            console.log('submitted data', data);
+        },
+    })
+);
