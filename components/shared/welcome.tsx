52eaf0d97c958a95a02cd04a754663d1677e38d6
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-22 17:51:58 +0100

    setup Welcome component

---
+++ components/shared/welcome.tsx
@@ -0,0 +1,24 @@
+import {useOnboardingAppState} from '@/store';
+
+const Welcome = () => {
+    const {nextStep} = useOnboardingAppState();
+    return (
+        <div className="step active">
+            <div className="welcome-content">
+                <h1>Welcome to Flowva</h1>
+                <p>
+                    Your smart library for organizing tools, tracking usage, and
+                    turning productivity into rewards. Let`&apos;s set up your
+                    digital library in 2 minutes.
+                </p>
+            </div>
+            <div className="btn-fob-group">
+                <button className="btn-fob" onClick={nextStep}>
+                    Let`&apos;s Go
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Welcome;
