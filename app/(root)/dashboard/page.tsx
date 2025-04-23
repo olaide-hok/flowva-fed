dad612170406f08130e18d789f4c0dbb68ebe790
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-23 22:16:35 +0100

    render UserProfileDetails

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,18 +1,13 @@
-'use client';
-
 import CustomSvgs from '@/components/shared/custom-svgs';
 import {APP_NAME} from '@/lib/constants';
-import {useOnboardingAppState} from '@/store';
-// import {Metadata} from 'next';
+import {Metadata} from 'next';
+import UserProfileDetails from './user-profile';
 
-// export const metadata: Metadata = {
-//     title: 'Dashboard',
-// };
+export const metadata: Metadata = {
+    title: 'Dashboard',
+};
 
 export default function Dashboard() {
-    const {onboardingData} = useOnboardingAppState();
-    console.log('onboarding data', onboardingData);
-
     return (
         <div className="container-fob animate__animated animate__fadeInUp">
             <div className="flex flex-center mb-[30px] font-bold text-2xl text-(--primary-fb)">
@@ -22,6 +17,7 @@ export default function Dashboard() {
             <div className="text-[22px] font-semibold mb-[30px] text-center text-(--gray-700-fb)">
                 {APP_NAME} Dashboard
             </div>
+            <UserProfileDetails />
         </div>
     );
 }
