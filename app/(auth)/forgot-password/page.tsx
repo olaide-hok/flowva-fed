88e1be4a69d41b1a25fac8bf91aa4e8922594d95
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-23 08:56:11 +0100

    remove commented code

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,7 +1,5 @@
 import {Metadata} from 'next';
 import {APP_NAME} from '@/lib/constants';
-// import {auth} from '@/auth';
-// import {redirect} from 'next/navigation';
 import CustomSvgs from '@/components/shared/custom-svgs';
 import ForgotPasswordForm from './forgot-password-form';
 
@@ -10,12 +8,6 @@ export const metadata: Metadata = {
 };
 
 const ForgotPassword = async () => {
-    // const session = await auth();
-
-    // if (session) {
-    //     return redirect('/');
-    // }
-
     return (
         <div className="container animate__animated animate__fadeInUp">
             <div className="flex flex-center mb-[30px] font-bold text-2xl text-(--primary-fb)">
