c022f0398572ea8bc0063316154b59e9747dfc41
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-23 08:15:04 +0100

    add type attribute and extra class name to 'Go to Dashboard' button

--- components/shared/completion.tsx
+++ components/shared/completion.tsx
@@ -10,7 +10,7 @@ const Completion = () => {
 
     return (
         <div className="step active">
-            <h2>Setup Complete!</h2>
+            <h2 className="font-bold">Setup Complete!</h2>
             <p>
                 Your Flowva library is ready to use. We&apos;ll take you to your
                 dashboard now where you can start organizing your tools and
@@ -18,7 +18,10 @@ const Completion = () => {
             </p>
 
             <div className="btn-fob-group">
-                <button className="btn-fob" onClick={handleFinish}>
+                <button
+                    type="button"
+                    className="btn-fob cursor-pointer"
+                    onClick={handleFinish}>
                     Go to Dashboard
                 </button>
             </div>
