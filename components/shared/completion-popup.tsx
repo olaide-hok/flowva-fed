c8bfbc5bb7542eaf1f842858df088765f8fbee4b
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-23 08:29:37 +0100

    add 'font-bold' utility class name to h2

--- components/shared/completion-popup.tsx
+++ components/shared/completion-popup.tsx
@@ -6,7 +6,7 @@ const CompletionPopup = ({onClose}: CompletionPopupProps) => {
         <>
             <div className="overlay active"></div>
             <div className="completion-popup active">
-                <h2>Onboarding Complete!</h2>
+                <h2 className="font-bold">Onboarding Complete!</h2>
                 <p>Taking you to your dashboard now.</p>
                 <button
                     type="button"
