c04d86e2c445937a5dda65de4e4270e67b1735b7
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-23 08:55:18 +0100

    setup schema for reset password

--- lib/validators.ts
+++ lib/validators.ts
@@ -26,6 +26,19 @@ export const forgotPasswordSchema = z.object({
     email: z.string().email('Invalid email address'),
 });
 
+// Schema for reset password
+export const resetPasswordSchema = z
+    .object({
+        password: z.string().min(8, 'Password must be at least 8 characters'),
+        confirmPassword: z
+            .string()
+            .min(8, 'Confirm password must be at least 8 characters'),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords don't match",
+        path: ['confirmPassword'],
+    });
+
 // Onboarding form schemas
 
 // Schema for About You Info
