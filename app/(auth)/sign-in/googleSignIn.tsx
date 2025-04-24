2bc72f3e0e80598f8b81efedb2c318b513b4af52
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-24 13:21:20 +0100

    refactor google sign in button

--- app/(auth)/sign-in/googleSignIn.tsx
+++ app/(auth)/sign-in/googleSignIn.tsx
@@ -1,47 +1,21 @@
 'use client';
 
-import {signIn} from '@/auth';
 import CustomSvgs from '@/components/shared/custom-svgs';
-import {Button} from '@/components/ui/button';
-import {getProviders, useSession} from 'next-auth/react';
-import {useEffect, useState} from 'react';
-
-// Infer the type from getProviders()
-type Providers = Awaited<ReturnType<typeof getProviders>>;
+import {googleSignIn} from '@/lib/actions/user.actions';
 
 const GoogleSignInButton = () => {
-    const {data: session} = useSession();
-    const [providers, setProviders] = useState<Providers | null>(null);
-    console.log(session);
-
-    useEffect(() => {
-        const setAuthProviders = async () => {
-            const res = await getProviders();
-            setProviders(res);
-        };
-
-        setAuthProviders();
-    }, []);
+    const handleSignIn = () => {
+        googleSignIn();
+    };
 
     return (
-        <div>
-            {!session &&
-                providers &&
-                Object.values(providers).map((provider, index) => {
-                    if (provider.id === 'google') {
-                        return (
-                            <Button
-                                type="button"
-                                key={index}
-                                onClick={() => signIn(provider.id)}
-                                className="btn btn-secondary">
-                                <CustomSvgs type="google" />
-                                Google
-                            </Button>
-                        );
-                    }
-                })}
-        </div>
+        <button
+            type="button"
+            onClick={handleSignIn}
+            className="btn btn-secondary cursor-pointer">
+            <CustomSvgs type="google" />
+            Google
+        </button>
     );
 };
 
