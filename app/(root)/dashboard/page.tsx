46bc0b9601d0bf900c8a4703ed2641dbab682f3c
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-24 09:41:10 +0100

    wrap dashboard logo with link tag to navigate back to home page
    render sign out button

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -2,22 +2,29 @@ import CustomSvgs from '@/components/shared/custom-svgs';
 import {APP_NAME} from '@/lib/constants';
 import {Metadata} from 'next';
 import UserProfileDetails from './user-profile';
+import Link from 'next/link';
+import SignOutButton from './sign-out-button';
 
 export const metadata: Metadata = {
     title: 'Dashboard',
 };
 
-export default function Dashboard() {
+export default async function Dashboard() {
     return (
         <div className="container-fob animate__animated animate__fadeInUp">
-            <div className="flex flex-center mb-[30px] font-bold text-2xl text-(--primary-fb)">
+            <Link
+                href="/"
+                target="_self"
+                className="flex flex-center mb-[30px] font-bold text-2xl text-(--primary-fb)">
                 <CustomSvgs type="logo" className="mr-2.5 w-7 h-7" />
                 {APP_NAME}
-            </div>
+            </Link>
             <div className="text-[22px] font-semibold mb-[30px] text-center text-(--gray-700-fb)">
                 {APP_NAME} Dashboard
             </div>
             <UserProfileDetails />
+            {/* Sign Out */}
+            <SignOutButton />
         </div>
     );
 }
